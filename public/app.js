14987155e5a0eedcdae9691dcbba2d576b5802ce
caulkerman <ih8blackwidowspiders@yahoo.com>
2015-12-14 15:46:36 -0700

    getting rid of all the rw's

--- public/app.js
+++ public/app.js
@@ -12,49 +12,49 @@ angular.module("timeCard", ["ui.router"])
 
 		.state("admin", {
 			url: "/admin",
-			templateUrl: "components/admin/rw-admin-template.html",
+			templateUrl: "components/admin/admin-template.html",
 			controller: "adminController"
 		})
 
 		.state("admin-employees", {
 			url: "/admin-employees",
-			templateUrl: "components/admin-employees/rw-admin-employees-template.html",
+			templateUrl: "components/admin-employees/admin-employees-template.html",
 			controller: "adminEmployeesController"
 		})
 
 		.state("admin-job-site", {
 			url: "/admin-job-site",
-			templateUrl: "conponents/admin-job-site/rw-admin-job-site-template.html",
-			controller: "rw-admin-job-site-controller"
+			templateUrl: "conponents/admin-job-site/admin-job-site-template.html",
+			controller: "admin-job-site-controller"
 		})
 
 		.state("admin-job-site-list", {
 			url: "/admin-job-site-list",
-			templateUrl: "components/admin-job-site-list/rw-admin-job-site-list-template.html",
+			templateUrl: "components/admin-job-site-list/admin-job-site-list-template.html",
 			controller: "adminJobSiteListController"
 		})
 
 		.state("employee-job-site-list", {
 			url: "/employee-job-site-list",
-			templateUrl: "components/employee-job-site-list/rw-employee-job-site-list-template.html",
+			templateUrl: "components/employee-job-site-list/employee-job-site-list-template.html",
 			controller: "employeeJobSiteListController"
 		})
 
 		.state("employee-job-site-time-card", {
 			url: "/employee-job-site-time-card",
-			templateUrl: "components/employee-job-site-time-card/rw-employee-job-site-time-card-template.html",
-			controller: "rw-employee-job-site-time-card-controller"
+			templateUrl: "components/employee-job-site-time-card/employee-job-site-time-card-template.html",
+			controller: "employee-job-site-time-card-controller"
 		})
 
 		.state("employee-time-card", {
 			url: "/employee-time-card",
-			templateUrl: "components/employee-time-card/rw-employee-time-card-template.html",
+			templateUrl: "components/employee-time-card/employee-time-card-template.html",
 			controller: "employeeTimeCardController"
 		})
 
 		.state("employee", {
 			url: "/employee",
-			templateUrl: "components/employee/rw-employee-template.html",
+			templateUrl: "components/employee/employee-template.html",
 			controller: "employeeController"
 		})
 

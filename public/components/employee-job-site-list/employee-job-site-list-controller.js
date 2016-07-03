bcb0732857f0423537220ea0b341abcc8b15733b
caulkerman <ih8blackwidowspiders@yahoo.com>
2016-07-02 21:19:18 -0600

    starting the employee jobsite object testing

--- public/components/employee-job-site-list/employee-job-site-list-controller.js
+++ public/components/employee-job-site-list/employee-job-site-list-controller.js
@@ -1,14 +1,43 @@
 (function() {
+var $inject = ["$scope"];
+function employeeJobSiteListControllerCB($scope) {
 
-	var $inject = ["$scope"];
+		////////ADD CODE BELOW\\\\\\\\\\
 
-	function employeeJobSiteListControllerCB($scope) {
-
-		$scope.test = "this is from employeeJobSiteListController";
+$scope.test = "this is from employeeJobSiteListController";
 	
+	$scope.jobSiteList = [];
+
+	var AddJob = function(job) {
+		this.name = job;
 	}
 
-	employeeJobSiteListControllerCB.$inject = $inject;
-	angular.module("timeCard").controller("employeeJobSiteListController", employeeJobSiteListControllerCB);
+
+//in the function below add a counter to AddJob and contatinate with +1 so each AddJob 
+//pushed to the array has a number added to it such as AddJob1, AddJob2, AddJob3, 
+//and so on.
+	$scope.pushToJobSiteList = function(job, cb) {
+		var addJob = new AddJob(job);
+		$scope.jobSiteList.push(addJob);
+		console.log($scope.jobSiteList);
+	}
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+}
+
+employeeJobSiteListControllerCB.$inject = $inject;
+angular.module("timeCard").controller("employeeJobSiteListController", employeeJobSiteListControllerCB);
 
 })();

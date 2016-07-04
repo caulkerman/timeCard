20f47b51a8c3424f654c2baecf772a26a42b53d6
caulkerman <ih8blackwidowspiders@yahoo.com>
2016-07-03 19:48:33 -0600

    getting the server up and going in preparation for sending data from front end to back end

--- server.js
+++ server.js
@@ -3,3 +3,34 @@ var cors = require("cors");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 
+
+//express
+var app = express();
+var router = express.Router();
+
+
+// Express Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({
+  limit: '50mb'
+}));
+
+// Endpoints
+
+
+// Connections
+var port = 9333;
+var mongoUri = 'mongodb://localhost:27017/timecard';
+// var mLabsPassword = require("./api/mLabsPassword/mLabsPassword")
+// var mongoUri = mLabsPassword.password;
+mongoose.set('debug', true);
+mongoose.connect(mongoUri);
+mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
+mongoose.connection.once('open', function() {  ////event listener
+  console.log('connected to mongoDB at: ', mongoUri);
+});
+
+app.listen(port, function() {
+  console.log('listening on port: ', port);
+});

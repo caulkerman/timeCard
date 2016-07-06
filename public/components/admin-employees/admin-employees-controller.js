6858b8600a3e44fee6ec99ab491fe82d421dd580
caulkerman <ih8blackwidowspiders@yahoo.com>
2016-07-05 18:56:00 -0600

    Working on admin-employees to create an employee object and to validate the html form so the object is complete with no properties having undefined values

--- public/components/admin-employees/admin-employees-controller.js
+++ public/components/admin-employees/admin-employees-controller.js
@@ -1,23 +1,58 @@
 (function() {
-var $inject = ["$scope", "admin_employees_service"];
-function adminEmployeesControllerCB($scope, admin_employees_service) {
+var $inject = ["$scope", "$log", "admin_employees_service"];
+function adminEmployeesControllerCB($scope, $log, admin_employees_service) {
 
 'use strict'
 		//////////    ADD YOUR CONTROLLER CODE BELOW   ///////////
 
 	$scope.employeeArray = admin_employees_service.employeeArray();
 
-	$scope.createEmployeeName = function(firstName, lastName, email) {
+	$scope.createEmployee = function(firstName, lastName, email, password) {
+		if ($scope.firstName === "" || $scope.firstName === undefined) {
+			$scope.firstNameWarning = true;
+		} else {
+			$scope.firstNameWarning = false;
+		};
+		
+		if ($scope.lastName === "" || $scope.lastName === undefined) {
+			$scope.lastNameWarning = true;
+		} else {
+			$scope.lastNameWarning = false;
+		};
+		
+		if ($scope.email === "" || $scope.email === undefined) {
+			$scope.emailWarning = true;
+		} else {
+			$scope.emailWarning = false;
+		};
 
-		function Employee(first, last, email) {
-			this.firstName = first;
-			this.lastName = last;
-			this.email = email;
+		if ($scope.password === "" || $scope.password === undefined) {
+			$scope.passwordWarning = true;
+		} else {
+			$scope.passwordWarning = false;
+		};
+		
+		var employee = {
+			firstName: firstName,
+			lastName: lastName,
+			email: email,
+			password: password
 		}
-		var employee = new Employee(firstName, lastName, email);
+
+		for (var prop in employee) {
+			if (employee[prop] === undefined || employee[prop] === "") {
+				$log.warn("hello");
+				return;
+			} else {
+				$log.error("I smell like butter");
+			} 
+		};
+
+		console.log("the employee object, ", employee);
+		
 			
-		admin_employees_service.newEmployeeNameObject(employee);
-		$scope.employeeArray = admin_employees_service.employeeArray();
+		// admin_employees_service.newEmployeeNameObject(firstName, lastName, email, password);
+		// $scope.employeeArray = admin_employees_service.employeeArray();
 	};
 
 		

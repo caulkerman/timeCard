45180169161785ec90083fcd1cf0b7e25ce26318
caulkerman <ih8blackwidowspiders@yahoo.com>
2016-10-16 20:54:40 -0600

    I have the modal code stripped down the way I want and comments deleted

--- public/components/login/login-controller.js
+++ public/components/login/login-controller.js
@@ -1,12 +1,24 @@
 (function() {
-var $inject = ["$scope", "$state"];
-function loginControllerCB($scope, $state) {
+var $inject = ["$scope", "$state", "$uibModal", "$log", "$document"];
+function loginControllerCB($scope, $state, $uibModal, $log, $document) {
 'use strict'
 const ctrl = this;
 
-ctrl.test = "this is a test";
-
 		////////ADD JAVASCRIPT BELOW////////	
+
+
+ctrl.animationsEnabled = true;
+   
+  ctrl.open = function (parentSelector) {
+    var modalInstance = $uibModal.open({
+      animation: ctrl.animationsEnabled,
+      templateUrl: 'myModalContent.html',
+      controller: 'ModalInstanceCtrl',
+      controllerAs: 'ctrl',
+    });
+  };
+
+
 }
 loginControllerCB.$inject = $inject;
 angular.module("timeCard").controller("loginController", loginControllerCB)
@@ -14,98 +26,25 @@ angular.module("timeCard").controller("loginController", loginControllerCB)
 
 
 
-
-
-
-
-
-///////////the modal js\\\\\\\\\\\\\\\\\\\\\\
-app.controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
-  var $ctrl = this;
-  // $ctrl.items = ['item1', 'item2', 'item3'];
-
-  $ctrl.animationsEnabled = true;
-  
-  
-  $ctrl.open = function (parentSelector) {
-    // var parentElem = parentSelector ? angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-    var modalInstance = $uibModal.open({
-      animation: $ctrl.animationsEnabled,
-      // ariaLabelledBy: 'modal-title',
-      // ariaDescribedBy: 'modal-body',
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl',
-      controllerAs: '$ctrl',
-      // size: size,
-      // appendTo: parentElem,
-      // resolve: {
-      //   items: function () {
-      //     return $ctrl.items;
-      //   }
-      // }
-    });
-
-    // modalInstance.result.then(function (selectedItem) {
-    //   $ctrl.selected = selectedItem;
-    // }, function () {
-    //   $log.info('Modal dismissed at: ' + new Date());
-    // });
-  };
-});
-
-
-
-
-// Please note that $uibModalInstance represents a modal window (instance) dependency.
-// It is not the same as the $uibModal service used above.
-
 app.controller('ModalInstanceCtrl', function ($uibModalInstance/*, items*/, $scope) {
-  var $ctrl = this;
-  // $ctrl.items = items;
-  // $ctrl.selected = {
-  //   item: $ctrl.items[0]
-  // };
+  var ctrl = this;
 
-  $scope.theInputValue;
+  ////////ADD YOUR JAVASCRIPT HERE\\\\\\\\
 
-  $scope.makeFunctionFire = function() {
-    console.log("this is the ModalInstanceCtrl controller. And this is the input value: ", $scope.theInputValue);
+  ctrl.theInputValue;
+  
+  ctrl.makeFunctionFire = function() {
+    console.log("this is the ModalInstanceCtrl controller. And this is the input value: ", ctrl.theInputValue);
   }
 
-  $ctrl.ok = function () {
-    $uibModalInstance.close(/*$ctrl.selected.item*/$scope.makeFunctionFire());
+  ctrl.ok = function () {
+    $uibModalInstance.close(/*$ctrl.selected.item*/ctrl.makeFunctionFire());//inside the close(parameters) you can put anything that needs to be executed and returned as the modal closes to be made available to the controller.
   };
 
-  $ctrl.cancel = function () {
-    $uibModalInstance.dismiss(/*'cancel'*/);
+  ctrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
   };
 });
 
 // Please note that the close and dismiss bindings are from $uibModalInstance.
 
-// app.component('modalComponent', {
-//   templateUrl: 'myModalContent.html',
-//   bindings: {
-//     resolve: '<',
-//     close: '&',
-//     dismiss: '&'
-//   },
-//   controller: function () {
-//     var $ctrl = this;
-
-//     $ctrl.$onInit = function () {
-//       $ctrl.items = $ctrl.resolve.items;
-//       $ctrl.selected = {
-//         item: $ctrl.items[0]
-//       };
-//     };
-
-//     $ctrl.ok = function () {
-//       $ctrl.close({$value: $ctrl.selected.item});
-//     };
-
-//     $ctrl.cancel = function () {
-//       $ctrl.dismiss({$value: 'cancel'});
-//     };
-//   }
-// });

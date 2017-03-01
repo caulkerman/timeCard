7e7aa0c477395c0f95fc6033700efcfc01efd921
Randy Williams <Randy Williams>
2017-02-28 21:52:20 -0800

    changes

--- public/components/employee-job-site-time-card/employee-job-site-time-card-controller.js
+++ public/components/employee-job-site-time-card/employee-job-site-time-card-controller.js
@@ -312,6 +312,7 @@ function createCustomId() {
 	var lettersArray = ["A", "a", "B", "b", "C", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"];
 	var numbersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 	for(var i = 0; i < 12; i++) {
+		
 		var letter = Math.floor(Math.random() * lettersArray.length);
 		var customIdLetter = lettersArray[letter];
 
@@ -328,6 +329,7 @@ function createCustomId() {
 
 
 function addAllTheHours() {
+
 	ctrl.totalJobSiteHours = 0;
 
 	for (var i = 0; i < dailyTCArray.length; i++) {

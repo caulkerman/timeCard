b0186269f00d92ecaf75ac9fd4dc4d795727d5bd
caulkerman <ih8blackwidowspiders@yahoo.com>
2017-09-29 18:05:22 -0600

    starting on the employee-time-card page, switching computers

--- public/components/employee-time-card/employee-time-card-controller.js
+++ public/components/employee-time-card/employee-time-card-controller.js
@@ -1,14 +1,46 @@
 (function() {
-	var $inject = ["$scope"];
+	var $inject = ["$scope", "admin_employees_list_service", "employeeJobSiteTimeCardService"];
 
-	function employeeTimeCardControllerCB($scope) {
+	function employeeTimeCardControllerCB($scope, admin_employees_list_service, employeeJobSiteTimeCardService) {
 
 'use strict'
+var ctrl = this;
 
 ///////ADD JAVASCRIPT BELOW////////
 
-
+ctrl.employees = [];
 $scope.test = "this is from employeeTimeCardController";
+
+(function() {
+	admin_employees_list_service.getEmployees().then(function(response) {
+		for (var i = 0; i < response.data.length; i++) {
+			if (response.data[i].employeeType === "Worker") {
+				ctrl.employees.push(response.data[i]);
+			};
+		};
+	    console.log("the employees object ", ctrl.employees);
+	    console.log("the response.data: ", response.data);
+	});
+})();
+
+ctrl.theDate = employeeJobSiteTimeCardService.theDate();
+console.log("the new theDate object: ", ctrl.theDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////END OF CONTROLLER\\\\\\\\
 
 	}
 

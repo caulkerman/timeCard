56c2accc3b3aae45f430a4cf09f2da153945df73
caulkerman <ih8blackwidowspiders@yahoo.com>
2017-11-09 20:43:53 -0700

    login inputs now clear out when unused login button is clicked

--- public/components/login/login-controller.js
+++ public/components/login/login-controller.js
@@ -8,16 +8,24 @@ const ctrl = this;
 		////////ADD JAVASCRIPT BELOW////////	
 
 
-ctrl.animationsEnabled = true;
+ctrl.loginClick = function() {
+  ctrl.username = "";
+  ctrl.password = "";
+}
+
+
+
+
+// ctrl.animationsEnabled = true;
    
-  ctrl.open = function (parentSelector) {
-    var modalInstance = $uibModal.open({
-      animation: ctrl.animationsEnabled,
-      templateUrl: 'myModalContent.html',
-      controller: 'ModalInstanceCtrl1',
-      controllerAs: 'ctrl',
-    });
-  };
+//   ctrl.open = function (parentSelector) {
+//     var modalInstance = $uibModal.open({
+//       animation: ctrl.animationsEnabled,
+//       templateUrl: 'myModalContent.html',
+//       controller: 'ModalInstanceCtrl1',
+//       controllerAs: 'ctrl',
+//     });
+//   };
 
 
 }
@@ -27,25 +35,25 @@ angular.module("timeCard").controller("loginController", loginControllerCB)
 
 
 ////////Modal Controller here\\\\\\\\\\\
-app.controller('ModalInstanceCtrl1', function ($uibModalInstance/*, items*/, $scope) {//make sure you change the name of your controller so as not to get controller conflicts
-  var ctrl = this;
+// app.controller('ModalInstanceCtrl1', function ($uibModalInstance/*, items*/, $scope) {//make sure you change the name of your controller so as not to get controller conflicts
+//   var ctrl = this;
 
-  ////////ADD YOUR JAVASCRIPT HERE\\\\\\\\
+//   ////////ADD YOUR JAVASCRIPT HERE\\\\\\\\
 
-  ctrl.theInputValue;
+//   ctrl.theInputValue;
   
-  ctrl.makeFunctionFire = function() {
-    console.log("this is the ModalInstanceCtrl controller. And this is the input value: ", ctrl.theInputValue);
-  }
+//   ctrl.makeFunctionFire = function() {
+//     console.log("this is the ModalInstanceCtrl controller. And this is the input value: ", ctrl.theInputValue);
+//   }
 
-  ctrl.ok = function () {
-    $uibModalInstance.close(/*$ctrl.selected.item*/ctrl.makeFunctionFire());//inside the close(parameters) you can put anything that needs to be executed and returned as the modal closes to be made available to the controller.
-  };
+//   ctrl.ok = function () {
+//     $uibModalInstance.close(/*$ctrl.selected.item*/ctrl.makeFunctionFire());//inside the close(parameters) you can put anything that needs to be executed and returned as the modal closes to be made available to the controller.
+//   };
 
-  ctrl.cancel = function () {
-    $uibModalInstance.dismiss('cancel');
-  };
-});
+//   ctrl.cancel = function () {
+//     $uibModalInstance.dismiss('cancel');
+//   };
+// });
 
 // Please note that the close and dismiss bindings are from $uibModalInstance.
 
